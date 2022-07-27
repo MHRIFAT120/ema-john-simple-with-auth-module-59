21d63d1851af62e459d2cbacb514a0fb44d2d6a9
MHRIFAT120 <mahamudulhoque92@gmail.com>
2022-07-27 14:18:04 +0600

    implement login and direct to shop

---
+++ src/__auth_steps.js
@@ -0,0 +1,22 @@
+/*
+  1.create a new firebase project in console.firebase.google.com
+  2.npm install firebase
+  3.create firebase.init.js and import getAuth and export Auth
+  4.Firebase setting > Authentication > enable Email and Password  auth
+  5.Create Login, SignUp component,setup route
+  6.attach from feild handler and form submit handler
+  7.npm install --save react-firebase-hooks
+  8.useCreateUserWithEmailAndPassword from react-firebase-hook
+  9.If user is created redirecting to the expected page
+  10.useSignInWithEmailAndPassword for sign in
+*/
+
+
+/*
+ Hosting steps
+ 1.npm install -g firebase-tools
+ 2.firebase login
+ 3.firebase init
+ 4.firebase deploy
+*/
+
